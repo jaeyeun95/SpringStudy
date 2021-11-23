70bce1461dc57621e22841042bb8cd40e370236c
jaeyeun95 <jaeyeun95@gmail.com>
2021-11-23 16:53:02 +0900

    Interceptor 와 Logback 추가, Logback 분석 필요

--- song_java/example-spring/src/main/java/com/study/sonjava/configuration/WebConfiguration.java
+++ song_java/example-spring/src/main/java/com/study/sonjava/configuration/WebConfiguration.java
@@ -2,12 +2,16 @@ package com.study.sonjava.configuration;
 
 import java.util.Locale;
 
+import com.study.sonjava.configuration.servlet.handler.BaseHandlerInterceptor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration implements WebMvcConfigurer {
 
     // 스프링에서 기본 제공해줌
     @Bean
@@ -19,7 +23,19 @@ public class WebConfiguration {
         source.setDefaultLocale(Locale.KOREAN);
         source.setUseCodeAsDefaultMessage(true);
         return source;
-
     }
+
+    @Bean
+    public BaseHandlerInterceptor baseHandlerInterceptor(){
+        return new BaseHandlerInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(baseHandlerInterceptor());
+    }
+
+    
     
 }
---
+++ song_java/example-spring/src/main/java/com/study/sonjava/configuration/servlet/handler/BaseHandlerInterceptor.java
@@ -0,0 +1,49 @@
+package com.study.sonjava.configuration.servlet.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class BaseHandlerInterceptor extends HandlerInterceptorAdapter {
+
+    /*
+	 * Interceptor 개요
+	 * 
+	 * HandlerInterceptor(-> HandlerInterceptorAdapter를 상속해서 구현)
+	 * 
+	 * - 세가지의 메소드를 오버라이딩해서 사용
+	 * 
+	 * 1. preHandle(전처리) : DispatcherServlet이 컨트롤러를 호출하기전
+	 * 2. postHandle(후처리): 컨트롤러에서 DispatcherServlet으로 리턴되는 순간(ModelAndView를 통해 작업결과 참조가능)
+	 * 3. afterCompletion(뷰단처리후) : 모든 뷰에서 최종 결과를 생성하는 일을 포함한 모든 작업이 완료된 후 
+	 *                              요청처리 중에 사용한 리소스를 반환해주기에 적당한 메소드
+	 */
+    Logger logger = LoggerFactory.getLogger(getClass());
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        // return super.preHandle(request, response, handler);
+        logger.info("preHandle requestURI : {} ", request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+        // super.postHandle(request, response, handler, modelAndView);
+        logger.info("postHandle request URI : {} ", request.getRequestURI());
+    }
+
+
+    
+
+
+
+    
+    
+}
