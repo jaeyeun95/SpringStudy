062b082e7b3e89a4e93d6cc074fdf5a9c36785a9
jaeyeun95 <jaeyeun95@gmail.com>
2021-11-26 14:41:06 +0900

    E16 JSP로 공통파일업로드 구현하기 JSP 설정은 했으나 .jsp 파일이 리턴되지 않음. 다시해야함

--- song_java/example-spring/src/main/java/com/study/sonjava/mvc/controller/FileController.java
+++ song_java/example-spring/src/main/java/com/study/sonjava/mvc/controller/FileController.java
@@ -1,12 +1,21 @@
 package com.study.sonjava.mvc.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.UUID;
+
 import com.study.sonjava.configuration.GlobalConfig;
+import com.study.sonjava.configuration.exception.BaseException;
 import com.study.sonjava.configuration.http.BaseResponse;
+import com.study.sonjava.configuration.http.BaseResponseCode;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,14 +28,37 @@ public class FileController {
     @Autowired
     private GlobalConfig config;
 
+    // @RequestMapping("/main")
+    // public String mainPage(){
+    //     return "index";
+    // }
+
     /**
      * 업로드 리턴
      */
-    @GetMapping
-    public BaseResponse<Boolean> save(){
-        log.debug("config : {} ", config);
+    @PostMapping("/save")
+    public BaseResponse<Boolean> save(@RequestParam("uploadFile") MultipartFile multipartFile ){
+        // log.debug("config : {} ", config);
+        log.debug("multipartFile : {} ", multipartFile);
+        if (multipartFile == null || multipartFile.isEmpty()){
+            throw new BaseException(BaseResponseCode.DATA_IS_NULL);
+        }
         String uploadFilePath = config.getUploadFilePath();
         log.debug("uploadFilePath : {}", uploadFilePath);
+        if (config.isProd()){
+            log.debug("isProd calendar : {}", Calendar.getInstance());
+        }
+        String prefix = multipartFile.getOriginalFilename().substring(multipartFile.getOriginalFilename().lastIndexOf("."), multipartFile.getOriginalFilename().length());
+        String filename = UUID.randomUUID().toString() + "." + prefix;
+        String pathname = uploadFilePath + filename;
+        File dest = new File(pathname);
+
+        try {
+            multipartFile.transferTo(dest);
+        } catch (IllegalStateException | IOException e) {
+            log.error("e", e);
+        }
+        
         return new BaseResponse<Boolean>(true);
     }
     
