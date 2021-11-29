264df6ac842f085e6b0be690591130804c36d002
jaeyeun95 <jaeyeun95@gmail.com>
2021-11-29 14:28:11 +0900

    API 샘플 추가 영화목록 조회 API 서비스 RestAPI에 샘플 코드 추가

---
+++ song_java/example-spring2/src/main/java/com/study/demo/restapi/RestAPI.java
@@ -0,0 +1,75 @@
+package com.study.demo.restapi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+@RestController
+public class RestAPI {
+
+    @GetMapping("/GetkobisData")
+    public String callAPI() throws JsonProcessingException {
+ 
+        HashMap<String, Object> result = new HashMap<String, Object>();
+ 
+        String jsonInString = "";
+ 
+        try {
+ 
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectTimeout(5000); //타임아웃 설정 5초
+            factory.setReadTimeout(5000);//타임아웃 설정 5초
+            RestTemplate restTemplate = new RestTemplate(factory);
+ 
+            HttpHeaders header = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(header);
+ 
+            String url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json";
+ 
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url+"?"+"key=f5eef3421c602c6cb7ea224104795888").build();
+ 
+            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+            ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
+            result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
+            result.put("header", resultMap.getHeaders()); //헤더 정보 확인
+            result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
+
+            System.out.println("######### "+result);
+ 
+            //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
+            ObjectMapper mapper = new ObjectMapper();
+            jsonInString = mapper.writeValueAsString(resultMap.getBody());
+ 
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body"  , e.getStatusText());
+            System.out.println("dfdfdfdf");
+            System.out.println(e.toString());
+ 
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body"  , "excpetion오류");
+            System.out.println(e.toString());
+        }
+ 
+        return jsonInString;
+ 
+    }
+    
+}
