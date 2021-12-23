f3fabd5eb7d9b6becfd01858895d96c7495d88de
jaeyeun <jaeyeun95@gmail.com>
2021-12-23 23:31:53 +0900

    rxProgramming 예제 추가

--- rx/src/main/java/com/example/chapter1/ToDoSample.java
+++ rx/src/main/java/com/example/chapter1/ToDoSample.java
@@ -1,19 +1,18 @@
 package com.example.chapter1;
 
 import io.reactivex.Observable;
-import io.reactivex.Scheduler;
 import io.reactivex.schedulers.Schedulers;
 
 public class ToDoSample {
     public static void main(String[] args) throws InterruptedException {
         Observable.just(100, 200, 300, 400, 500)
             .doOnNext(data -> System.out.println(getThreadName() + " : " + "#doOnNext() : " + data))
-            // .subscribe(Scheduler.io())
-            // .observeOn(Schedulers.computation())
+            .subscribeOn(Schedulers.io())   // main thread 가 아닌 곳에서 실행된다.
+            .observeOn(Schedulers.computation())
             .filter(number -> number > 300)
             .subscribe(num -> System.out.println(getThreadName() + " : result : " + num));
 
-            // Thread.sleep(500);
+            Thread.sleep(500);
     }
 
     public static String getThreadName() {
