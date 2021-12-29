c3890e81407399f5c5cb9b8801e71acf81d8dbef
jaeyeun95 <jaeyeun95@gmail.com>
2021-12-29 10:22:24 +0900

    스프링 DB 접근 기술3 스프링 JdbcTemplate 오류 해결 JPA 사용'

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -21,10 +21,10 @@ public class MemberServiceIntegrationTest {
 
     // 테스트는 한글로 해도 무방하다
     @Test
-    void 회원가입(){
+    public void 회원가입() throws Exception {
         // given
         Member member = new Member();
-        member.setName("spring");
+        member.setName("hello");
 
         // when
         Long saveId = memberService.join(member);
@@ -36,7 +36,7 @@ public class MemberServiceIntegrationTest {
     }
 
     @Test
-    public void 중복_회원_예외(){
+    public void 중복_회원_예외() throws Exception{
         // given
         Member member1 = new Member();
         member1.setName("spring");
@@ -46,21 +46,12 @@ public class MemberServiceIntegrationTest {
 
         // when
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2)); // 예외가 발생해야 한다.
 
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
 
     }
 
-    @Test
-    void findMembers(){
-
-    }
-
-    @Test
-    void findOne(){
-
-    }
     
 }
